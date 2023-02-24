39478fab2cea8ab92a7a45bab88d9700656f4985
jonghwa <yjw001206@gmail.com>
2023-02-24 17:55:08 +0900

    1. 파일 분리
    2. 최적화를 위해 눈, 비를 따로 저장하는게 아닌 강수 를 저장하는 것으로 변경.

---
+++ public/scripts/snowflake.js
@@ -0,0 +1,37 @@
+import precipitation from './precipitation.js';
+import droplet from './droplet.js';
+
+// Create a Snowflake class
+export default class Snowflake extends precipitation {
+    constructor(canvas,ctx,weatherEffectObj) {
+      super(canvas,ctx,weatherEffectObj);
+      this.size = Math.random() * 3 + 1;
+      this.speed = Math.random() + 0.5;
+      this.weight = Math.random()*0.1 + 0.5;
+      
+    }
+  
+    groundfall(canvas){
+  
+      this.y = canvas.height;
+      this.collided = true;
+      this.weatherEffectObj.snowflakesOnGround.push(this);
+      this.checkCollision(this,this.weatherEffectObj);
+  
+    }
+  
+  
+  
+    draw() {
+  
+      const ctx = this.ctx;
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.95)';
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+
+
+  
+  }
+  
